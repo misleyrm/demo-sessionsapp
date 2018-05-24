c5f6baf90eda709c96d6ccdfed497c8c4d04bd0b
misleyrm <misley.rodriguez@yahoo.com>
2018-05-24 14:40:13 -0400

    Carrierwave work on Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,35 @@
 # Carrierwave work on Heroku
-CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
+# config/initializers/carrierwave.rb
 
+CarrierWave.configure do |config|
   config.fog_credentials = {
+    # Configuration for Amazon S3 should be made available through an Environment variable.
+    # For local installations, export the env variable through the shell OR
+    # if using Passenger, set an Apache environment variable.
+    #
+    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
+    #
+    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
+
+    # Configuration for Amazon S3
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
-    :region                 => ENV['AWS_REGION'],                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    :region                 => ENV['AWS_REGION']
   }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']                             # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-end
 
-#
-# Paperclip::Attachment.default_options[:storage] = :s3
-# Paperclip::Attachment.default_options[:s3_protocol] = 'https'
-# Paperclip::Attachment.default_options[:s3_credentials] =
-#   { :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-#     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-#     :bucket => ENV['S3_BUCKET_NAME'],
-#     :s3_region => ENV['AWS_REGION']
-#    }
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
+  config.fog_directory    = ENV['S3_BUCKET_NAME']
+  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+end
