8cf270578cc3552fbcd27fb75a4bd1f60cd9ec59
misleyrm <misley.rodriguez@yahoo.com>
2018-05-24 09:09:57 -0400

    cache_dir configuration for Carrierwave work on Heroku

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,12 +2,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
 
-
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
   # Override the directory where uploaded files will be stored.
-    # This is a sensible default for uploaders that are meant to be mounted:
-    def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   version :thumb do
     process :crop
