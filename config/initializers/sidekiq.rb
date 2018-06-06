dee087118f04fe2319a7f8b90c61fa3aa81bc50f
misleyrm <misley.rodriguez@yahoo.com>
2018-06-06 09:18:00 -0400

    Sidekiq.configure

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/12' }
+  config.redis = { url: 'redis://h:p79df61a8ef69dadf4343b2505cdbae27004a8ee8b2a24adcb8f86783a75b4b04@ec2-52-2-220-105.compute-1.amazonaws.com:9789' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/12' }
+  config.redis = { url: 'redis://h:p79df61a8ef69dadf4343b2505cdbae27004a8ee8b2a24adcb8f86783a75b4b04@ec2-52-2-220-105.compute-1.amazonaws.com:9789' }
 end
