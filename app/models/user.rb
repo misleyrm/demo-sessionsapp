29909c8abd0274ef5d127f17b84343d27330e1ce
misleyrm <misley.rodriguez@yahoo.com>
2018-06-07 16:30:55 -0400

    user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   mount_uploader :image, AvatarUploader
   serialize :images, JSON # If you use SQLite, add this line.
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-  after_update :crop_avatar, :if => lambda { |o| o.crop_x.present? || o.crop_y.present? || o.crop_w.present? || o.crop_h.present? }
+  after_save :crop_avatar, :if => lambda { |o| o.crop_x.present? || o.crop_y.present? || o.crop_w.present? || o.crop_h.present? }
 
  def crop_avatar
    image.recreate_versions! if crop_x.present?
@@ -14,20 +14,22 @@ class User < ApplicationRecord
   # validates_presence_of :first_name, :if => lambda { |o| o.current_step == "personal" || o.current_step == steps.first }
   validates_presence_of :image, :if => lambda { |o| o.current_step == "avatar" || o.current_step == steps.first }
 
-  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :first_name, presence: true, length: { maximum: 50 },:if => lambda { |o| o.current_step != "avatar" || o.current_step != steps.first }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, length: { maximum: 255 },
   format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false } ,:if => lambda { |o| o.current_step != "avatar" || o.current_step != steps.first }
+  uniqueness: { case_sensitive: false }, :on => :create
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }, :if => lambda { |o| o.current_step == "security" ||  o.current_step == "createAccount" }
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
+  validates_presence_of :email, :on => :create
+  validates_presence_of :first_name, :on => :create
   validates_presence_of :current_password, :on => :updateEmail
   validates_presence_of :current_password, :on => :updatePassword
-  before_save :downcase_email
-  validates :image, presence: true
+  # before_save :downcase_email
+  # validates :image, presence: true
 
   before_create :create_activation_digest
 
@@ -341,7 +343,7 @@ class User < ApplicationRecord
     if (self.previous_changes.key?(:image) &&
        self.previous_changes[:image].first != self.previous_changes[:image].last)
        status = 'changeavatar'
-       ActionCable.server.broadcast 'user_channel', status: status, user: self.id, image: self.image.url, name: self.first_name
+       ActionCable.server.broadcast 'user_channel', status: status, user: self.id, image: self.image_url(:thumb), name: self.first_name
     elsif (self.previous_changes.key?(:email) &&
           self.previous_changes[:email].first != self.previous_changes[:email].last)
           status = 'changeemail'
